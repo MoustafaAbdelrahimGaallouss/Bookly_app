bc312bd40dc12796b80ec0f45f4523524a37ff7d
MoustafaAbdelrahimGaallouss <mga3llouss@gmail.com>
2025-11-10 00:23:59 +0200

    Add custom app bar widget to home page

--- lib/Features/Splash/presentation/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/widgets/splash_view_body.dart
@@ -1,8 +1,34 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +38,50 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 20),
-        Text(
-          'Read free books',
-          textAlign: TextAlign.center,
-          style: Theme.of(
-            context,
-          ).textTheme.displayMedium!.copyWith(fontSize: 24),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: Text(
+                'Read free books',
+                textAlign: TextAlign.center,
+                style: Theme.of(
+                  context,
+                ).textTheme.displayMedium!.copyWith(fontSize: 24),
+              ),
+            );
+          },
         ),
       ],
     );
+  }
+
+  ///Functions
+  ///==========================================
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  ///==========================================
+
+  void navigateToHome() {
+    Future.delayed(Duration(seconds: 2), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fadeIn,
+        duration: kTabScrollDuration,
+      );
+    });
   }
 }
---
+++ lib/Features/home/presentation/widgets/home_view_body.dart
@@ -0,0 +1,39 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [const CustomAppBar()]);
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo, height: 20),
+          IconButton(
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // Handle search button press
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
